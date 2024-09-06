10f7fd70643214389aad83c20d80ea70ea22e803
SamStar3 <samstar1044948@gmail.com>
2024-09-06 20:33:43 +0530

    First commit

---
+++ src/component/list/chatLIst/ChatList.jsx
@@ -0,0 +1,12 @@
+import "./chatList.css"
+
+const ChatList =() => {
+    return (
+        <div className="chatList">ChatList</div>
+    )
+}
+
+export default ChatList 
+
+// 19.30
+
---
+++ src/component/list/userInfo/UserInfo.jsx
@@ -0,0 +1,9 @@
+import "./UserInfo.css"
+
+const Userinfo =() => {
+    return (
+        <div className="userInfo">Userinfo</div>
+    )
+}
+
+export default Userinfo
