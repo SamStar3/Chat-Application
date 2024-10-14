a1ac0d26a3e3c85ac5c5a337180c0af2ecbdcd57
SamStar3 <samstar1044948@gmail.com>
2024-10-14 16:14:02 +0530

    filea added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useEffect } from "react";
-import Chat from "./component/chat/Chat";
-import Detail from "./component/detail/Detail";
-import AddUser from "./component/list/chatLIst/addUser/AddUser";
-import List from "./component/list/List";
-import Login from "./component/login/Login";
-import Notification from "./component/notification/Notification";
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import List from "./components/list/List";
+import Login from "./components/login/Login";
+import Notification from "./components/notification/Notification";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
 import { useUserStore } from "./lib/userStore";
+import { useChatStore } from "./lib/chatStore";
 
 const App = () => {
   const { currentUser, isLoading, fetchUserInfo } = useUserStore();
-  const { chatId } = useUserStore();
+  const { chatId } = useChatStore();
 
   useEffect(() => {
     const unSub = onAuthStateChanged(auth, (user) => {
--- src/lib/userStore.js
+++ src/lib/userStore.js
@@ -1,29 +1,25 @@
-import { create } from 'zustand';
-import { db } from './firebase';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc } from "firebase/firestore";
+import { create } from "zustand";
+import { db } from "./firebase";
 
 export const useUserStore = create((set) => ({
-  currentUser: null, 
+  currentUser: null,
   isLoading: true,
   fetchUserInfo: async (uid) => {
-    if (!uid) return set({ currentUser: null, isLoading: false }); // Fixed typo here as well
-
-    // set({ isLoading: true }); // Set loading to true before fetching
+    if (!uid) return set({ currentUser: null, isLoading: false });
 
     try {
       const docRef = doc(db, "users", uid);
       const docSnap = await getDoc(docRef);
 
       if (docSnap.exists()) {
-        set({ currentUser: docSnap.data(), isLoading: false }); // Set user data if document exists
-        
+        set({ currentUser: docSnap.data(), isLoading: false });
       } else {
-        set({ currentUser: null, isLoading: false }); // If no document, set currentUser to null
+        set({ currentUser: null, isLoading: false });
       }
-
     } catch (err) {
       console.log(err);
-      return set({ currentUser: null, isLoading: false }); // In case of error, reset currentUser and isLoading
+      return set({ currentUser: null, isLoading: false });
     }
-  }
+  },
 }));
