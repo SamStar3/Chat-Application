6e06d8f08c97b5c34c4bcd62a1e11e6db8bd70ee
SamStar3 <samstar1044948@gmail.com>
2024-10-17 23:33:38 +0530

    file added

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,11 +5,11 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "reactchat-8953e.firebaseapp.com",
-  projectId: "reactchat-8953e",
-  storageBucket: "reactchat-8953e.appspot.com",
-  messagingSenderId: "989490756392",
-  appId: "1:989490756392:web:9e54e92c7b39c531b82e1d"
+  authDomain: "reactchat-fffcf.firebaseapp.com",
+  projectId: "reactchat-fffcf",
+  storageBucket: "reactchat-fffcf.appspot.com",
+  messagingSenderId: "911035505084",
+  appId: "1:911035505084:web:aec3a8b19e46fbc89333b9"
 };
 
 const app = initializeApp(firebaseConfig);
