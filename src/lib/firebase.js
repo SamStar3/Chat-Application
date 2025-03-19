2cda86c8ae35f5e74ad2fbf46070cb5b98020982
SamStar3 <samstar1044948@gmail.com>
2025-03-19 08:35:31 +0530

    updated

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,18 +1,38 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth";  
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   authDomain: "reactchat-fffcf.firebaseapp.com",
+//   projectId: "reactchat-fffcf",
+//   storageBucket: "reactchat-fffcf.appspot.com",
+//   messagingSenderId: "911035505084",
+//   appId: "1:911035505084:web:aec3a8b19e46fbc89333b9"
+// };
+
+// const app = initializeApp(firebaseConfig);
+
+// export const auth = getAuth()
+// export const db = getFirestore()
+// export const storage = getStorage()
+
+// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "reactchat-fffcf.firebaseapp.com",
-  projectId: "reactchat-fffcf",
-  storageBucket: "reactchat-fffcf.appspot.com",
-  messagingSenderId: "911035505084",
-  appId: "1:911035505084:web:aec3a8b19e46fbc89333b9"
+  authDomain: "react-chat-64404.firebaseapp.com",
+  projectId: "react-chat-64404",
+  storageBucket: "react-chat-64404.firebasestorage.app",
+  messagingSenderId: "1020611457422",
+  appId: "1:1020611457422:web:b2b25c0bd9be224f3274df"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth()
